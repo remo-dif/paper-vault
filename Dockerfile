a074b8fe543392f634186d8c1a0e248955d56f37
remo-dif <remo.difcc@gmail.com>
2025-05-23 16:13:24 +0200

    refactor: update Docker configuration, enhance .gitignore, and improve application structure with environment variables

--- Dockerfile
+++ Dockerfile
@@ -13,14 +13,14 @@ RUN npm install
 # Bundle app source
 COPY . .
 
-# Copy the .env and .env.development files
+# Copy the .env file to the working directory
 COPY .env ./
 
 # Creates a "dist" folder with the production build
 RUN npm run build
 
 # Expose the port on which the app will run
-EXPOSE 3001
+EXPOSE 8080
 
-# Start the server using the production build
-CMD ["npm", "run", "start:dev"]
+# Run the app
+CMD [ "node", "--nolazy", "dist/main" ]
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,22 +3,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PapersModule } from './papers/papers.module';
-
-@Module({
-  imports: [
-    PapersModule,
-    TypeOrmModule.forRoot({
-      type: process.env.TYPEORM_TYPE as any,
-      url: process.env.TYPEORM_URL,
-      host: process.env.TYPEORM_HOST || 'localhost', // resolves to "db" in Docker
-      username: process.env.TYPEORM_USERNAME,
-      password: process.env.TYPEORM_PASSWORD,
-      database: process.env.TYPEORM_DATABASE,
-      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-      synchronize: process.env.TYPEORM_SYNCHRONIZE === 'true',
-    }),
-  ],
-  controllers: [AppController],
-  providers: [AppService],
-})
-export class AppModule {}
+import { ConfigModule } from '@nestjs/config';
+  @Module({
+    imports: [
+      PapersModule,
+      ConfigModule.forRoot({ isGlobal: true }),
+      TypeOrmModule.forRoot({
+        type: 'postgres',
+        url: process.env.TYPEORM_URL,
+        autoLoadEntities: true,
+        synchronize: process.env.TYPEORM_SYNCHRONIZE === 'true',
+      }),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
+  })
+  export class AppModule {};
